c8e4f74f0c05154033164934d3e01b5d2e8953d6
Pepivsky <ironglas68@gmail.com>
2020-11-01 13:14:30 -0600

    Ejemplo de uso de las funciones de orden superior

---
+++ src/model/EjemploFuncionDeOrdenSuperior.kt
@@ -0,0 +1,21 @@
+package model
+//funcion de orden superior que recibe otra funcion
+fun calculadora(a: Int, b: Int, c: Int, funcion: (Int, Int, Int) -> Int): Int {
+    return funcion(a, b, c) //pasando los parametros a la funcion
+}
+//funciones de una sola linea
+fun sumar(a: Int, b: Int, c: Int) = a + b + c
+
+fun restar(a: Int, b: Int, c: Int) = a - b - c
+
+fun multplicar(a: Int, b: Int, c: Int) = a * b * c
+
+
+fun main() {
+    //llamando a la funcion superior y pasandole otra funcion
+    println("la suma es: ${calculadora(2, 3, 4, ::sumar)}")
+    println("la resta es: ${calculadora(2, 4, 5, ::restar)}")
+    println("la multiplicacion es: ${calculadora(2, 2, 2, ::multplicar)}")
+
+
+}
---
+++ src/model/FuncionesDeOrdenSuperior.kt
@@ -0,0 +1,30 @@
+package model
+
+//Las funciones de orden superior son aquelllas  que reciben otra funcion como parametro
+
+//funcion de orden superior que recibe otra funcion y esa funcion reciobe dos enteros, se indica antes de la flecha, y despues de la flecha va el tipo de dato que regresa
+fun operar(v1: Int, v2: Int, fn: (Int, Int) -> Int) : Int {
+    return fn(v1, v2)
+}
+
+fun sumar(x1: Int, x2: Int) = x1 + x2
+
+fun restar(x1: Int, x2: Int) = x1 - x2
+
+fun multiplicar(x1: Int, x2: Int) = x1 * x2
+
+fun dividir(x1: Int, x2: Int) = x1 / x2
+
+fun main(parametro: Array<String>) {
+    val resu1 = operar(10, 5, ::sumar) //llamando a la funcion de orden superior y asignando el valor a una variable
+    println("La suma de 10 y 5 es $resu1")
+
+    val resu2 = operar(5, 2, ::sumar)
+    println("La suma de 5 y 2 es $resu2")
+
+    println("La resta de 100 y 40 es ${operar(100, 40, ::restar)}") //llamando directamente a al funcion
+
+    println("El producto entre 5 y 20 es ${operar(5, 20, ::multiplicar)}")
+
+    println("La división entre 10 y 5 es ${operar(10, 5, ::dividir)}")
+}
