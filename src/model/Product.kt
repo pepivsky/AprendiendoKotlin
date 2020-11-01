60316b583d46384174042df83883a21f44940f19
Pepivsky <ironglas68@gmail.com>
2020-11-01 10:21:33 -0600

    Se convirtio Product.kt en una clase abstracta y sus metodos

--- src/model/Product.kt
+++ src/model/Product.kt
@@ -1,24 +1,24 @@
 package model
 //open para permite que esta clase pueda ser heredada
 //las clases y metodo estan cerrados por defecto
-open class Product(var name: String, var description: String, var sku: Int) {
-
+abstract class Product(var name: String, var description: String, var sku: Int) {
+//no se puede creaar una instancia de una clase abstracta
     override fun toString(): String {
         return "Name: $name \nDescription: $description \nSKU: $sku"
     }
 
-    open fun create(): String {
+    /*open fun create(): String { //los metodos tambien deben ser open para poder sobreescribiros
         return "Create"
-    }
-    open fun read(): String {
-        return "Read"
-    }
-    open fun update(): String {
-        return "Update"
-    }
-    open fun delete(): String {
-        return "Delete"
-    }
+    }*/
+
+    //convirietiendo a metodos abstractos
+    abstract fun create(): String //metodo abstracto sin implementacion
+
+    abstract fun read(): String
+
+    abstract fun update(): String
+
+    abstract fun delete(): String
 
 
 }
