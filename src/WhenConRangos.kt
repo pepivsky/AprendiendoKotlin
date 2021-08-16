57132b9d94e897fe22e28e493ab3dd12fb90731e
pepivsky <ironglas68@gmail.com>
2021-08-16 10:24:26 -0500

    otra solucion agregada

--- src/WhenConRangos.kt
+++ src/WhenConRangos.kt
@@ -36,3 +36,19 @@ fun main() {
         }
     )
 }
+
+/*
+Solucion con inline function
+fun main() = when (readLine()!!.toInt()) {
+    in Int.MIN_VALUE..0 -> "no army"
+    in 1..4 -> "few"
+    in 5..9 -> "several"
+    in 10..19 -> "pack"
+    in 20..49 -> "lots"
+    in 50..99 -> "horde"
+    in 100..249 -> "throng"
+    in 250..499 -> "swarm"
+    in 500..999 -> "zounds"
+    in 1000..Int.MAX_VALUE -> "legion"
+    else -> "impossible" }.let(::print)
+ */
